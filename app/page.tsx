7dec244d53be71a065016bdfe0c72894b17abe13
ValentinGTrapaga <gonzaleztrapagav@gmail.com>
2023-10-27 15:53:49 -0300

    bugfix screen width

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import { NavBar } from './components/NavBar/NavBar'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between w-screen">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <NavBar />
       <Section classes="justify-evenly">
         <Title />
